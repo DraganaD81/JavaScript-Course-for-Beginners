881305e65ebc51dbbc9033f18f7ce4971b638694
DraganaD81 <dragana@twognation.com>
2025-02-03 21:06:14 +0100

    JavaScript Course for Beginners 3 commit

--- index.js
+++ index.js
@@ -1,11 +1,3 @@
-let name = "Mosh";
-console.log(name);
-
-// Cannot be a reserved keyword
-// Should be meaningful
-// Cannot start with a number (1name)
-// Cannot contain a space or hyphen (-)
-// Are case-sensitive
-
-let firstName = "Mosh";
-let lastName = "Hamedani";
+const interestRate = 0.3;
+interestRate = 1;
+console.log(interestRate);
