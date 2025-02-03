aeb1a6f63f164f503b39a858aaf80cb113be5a7a
DraganaD81 <dragana@twognation.com>
2025-02-03 21:30:08 +0100

    JavaScript Course for Beginners Objects

--- index.js
+++ index.js
@@ -1,5 +1,13 @@
-let name = "Mosh"; // String Literal
-let age = 30; // Number Literal
-let isApproved = false; // Boolean Literal
-let firstName = undefined;
-let selectedColor = null;
+let person = {
+  name: "Mosh",
+  age: 30,
+};
+
+// Dot Notation
+person.name = "John";
+
+// Bracket Notation
+let selection = "name";
+person[selection] = "Mary";
+
+console.log(person.name);
