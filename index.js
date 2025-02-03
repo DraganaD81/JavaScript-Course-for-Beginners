d58f947564207c2acd042a9de2f5c60aaf689742
DraganaD81 <dragana@twognation.com>
2025-02-03 21:39:48 +0100

    JavaScript Course for Beginners Arrays

--- index.js
+++ index.js
@@ -1,13 +1,3 @@
-let person = {
-  name: "Mosh",
-  age: 30,
-};
-
-// Dot Notation
-person.name = "John";
-
-// Bracket Notation
-let selection = "name";
-person[selection] = "Mary";
-
-console.log(person.name);
+let selectedColors = ["red", "blue"];
+selectedColors[2] = 1;
+console.log(selectedColors.length);
