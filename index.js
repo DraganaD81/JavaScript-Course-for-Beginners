6da65b039ece745cddb89d9ed4d8a32cadd6487e
DraganaD81 <dragana@twognation.com>
2025-02-03 22:09:32 +0100

    JavaScript Course for Beginners Types of Functions

--- index.js
+++ index.js
@@ -1,5 +1,11 @@
+// Performing a task
 function greet(name, lastName) {
   console.log("Hello " + name + " " + lastName);
 }
 
-greet("John", "Smith");
+// Calculating a value
+function square(number) {
+  return number * number;
+}
+
+console.log(square(2));
